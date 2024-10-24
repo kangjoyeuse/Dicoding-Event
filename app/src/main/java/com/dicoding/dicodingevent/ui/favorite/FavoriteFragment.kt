f4f564b8d9c1c787ee1d183d6ad111f774100541
Kangjoyeuse <120604169+kangjoyeuse@users.noreply.github.com>
2024-10-25 02:19:14 +0700

    add fragment and add navigation to favorite and  settings

---
+++ app/src/main/java/com/dicoding/dicodingevent/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,33 @@
+package com.dicoding.dicodingevent.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dicoding.dicodingevent.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Initialize your views and set up your logic here
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
